20cf997b847dc89e30f527646267e19ee6f3a2bb
josegeraldoge <josegeraldoge@hotmail.com>
2019-05-15 12:07:48 -0300

    Correção de Versões e Implementação de segurança

--- src/main/java/com/graveto/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/graveto/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -31,7 +31,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento pelo ID {}", id);
-		return this.lancamentoRepository.findById(id);
+		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
 	}
 
 	@CachePut("lancamentoPorId")
@@ -42,7 +42,7 @@ public class LancamentoServiceImpl implements LancamentoService {
 
 	public void remover(Long id) {
 		log.info("Removendo o lançamento ID {}", id);
-		this.lancamentoRepository.deleteById(id);
+		this.lancamentoRepository.delete(id);
 	}
 
 }
